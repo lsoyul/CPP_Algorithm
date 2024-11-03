fdd18eec23dae0828b7e46a2467b183cda2c48f9
trycatch <sgykim36@gmail.com>
2024-11-03 12:28:38 +0900

    Binary Tree maze generate
    - implement player / start,end tiles
    - update logic initialize

--- Algorithm/Maze/Maze.cpp
+++ Algorithm/Maze/Maze.cpp
@@ -1,9 +1,17 @@
 ﻿#include "pch.h"
-#include "ConsoleHelper.h"
+#include "Board.h"
+#include "Player.h"
+
+Board board;
+Player player;
 
 int main()
 {
-    const char* TILE = "■";
+    ::srand(static_cast<unsigned>(time(nullptr)));
+
+    board.Init(25, &player);
+    player.Init(&board);
+
     uint64 lastTick = 0;
     while (true)
     {
@@ -17,22 +25,9 @@ int main()
 
 
         // 2. Logic
-
+        player.Update(deltaTick);
 
         // 3. Rendering
-
-        ConsoleHelper::SetCursorPosition(0, 0);
-        ConsoleHelper::ShowConsoleCursor(false);
-        ConsoleHelper::SetCursorColor(ConsoleColor::RED);
-
-        for (int32 y = 0; y < 25; y++)
-        {
-            for (int32 x = 0; x < 25; x++)
-            {
-                cout << TILE;
-            }
-
-            cout << endl;
-        }
+        board.Render();
     }
 }
