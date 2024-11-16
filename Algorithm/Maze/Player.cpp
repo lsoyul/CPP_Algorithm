c3f1acd17a375258ee3c020537897fdbd4d510a5
trycatch <sgykim36@gmail.com>
2024-11-16 15:17:28 +0900

    Implements AStar path finding..

--- Algorithm/Maze/Player.cpp
+++ Algorithm/Maze/Player.cpp
@@ -10,13 +10,18 @@ void Player::Init(Board* board)
 	_board = board;
 
 	//RightHand();
-	Bfs();
+	//Bfs();
+	AStar();
 }
 
 void Player::Update(uint64 deltaTick)
 {
 	if (_pathIndex >= _path.size())
+	{
+		_board->GenerateMap();
+		Init(_board);
 		return;
+	}
 
 	_sumTick += deltaTick;
 	if (_sumTick >= MOVE_TICK)
@@ -162,6 +167,155 @@ void Player::Bfs()
 	// 거꾸로 거슬러 올라가기
 	pos = dest;
 	
+	while (true)
+	{
+		_path.push_back(pos);
+
+		// 시작점은 자신이 곧 부모임.
+		if (pos == parent[pos])
+			break;
+
+		pos = parent[pos];
+	}
+
+	std::reverse(_path.begin(), _path.end());
+}
+
+struct PQNode
+{
+	bool operator<(const PQNode& other) const { return f < other.f; }
+	bool operator>(const PQNode& other) const { return f > other.f; }
+
+	int32	f;		// f = g + h
+	int32	g;
+	Pos		pos;
+};
+
+void Player::AStar()
+{
+	// 점수 매기기
+	// F = G + H
+	// F = 최종 점수 (작을수록 좋음, 경로에 따라서 달라짐)
+	// G = 시작점에서 해당 좌표까지 이동하는데 드는 비용 (작을수록 좋음, 경로에 따라서 달라짐)
+	// H = 목적지에서 얼마나 가까운지? (작을수록 좋음, 고정값)
+
+	Pos start = _pos;
+	Pos dest = _board->GetExitPos();
+
+	enum
+	{
+		DIR_COUNT = 8	//4 or 8(consider diagonal)
+	};
+
+	Pos front[] =
+	{
+		Pos {-1, 0},	// UP
+		Pos {0, -1},	// LEFT
+		Pos {1, 0},		// DOWN
+		Pos {0, 1},		// RIGHT
+		Pos {-1, -1},	// UP_LEFT
+		Pos {1, -1},	// DOWN_LEFT
+		Pos {1, 1},		// DOWN_RIGHT
+		Pos {-1, 1},	// UP_RIGHT
+	};
+
+	int32 cost[] =
+	{
+		10,		// UP
+		10,		// LEFT
+		10,		// DOWN
+		10,		// RIGHT
+		14,		// UP_LEFT
+		14,		// DOWN_LEFT
+		14,		// DOWN_RIGHT
+		14		// UP_RIGHT
+	};
+	
+	const int32 size = _board->GetSize();
+
+	// ClosedList
+	// closed[y][x] -> (y, x) 에 방문을 했는지 여부
+	vector<vector<bool>> closed(size, vector<bool>(size, false));
+
+	// best[y][x] -> 지금까지 (y, x)에 대한 가장 좋은 비용 (작을수록 좋음)
+	vector<vector<int32>> best(size, vector<int32>(size, INT32_MAX));
+
+	// 부모 추적 용도
+	map<Pos, Pos> parent;
+
+	// OpenList
+	priority_queue<PQNode, vector<PQNode>, greater<PQNode>> pq;
+
+
+	// 1) 예약(발견) 시스템 구현
+	// -- 이미 더 좋은 경로를 찾았다면 스킵!
+	// 2) 뒤늦게 더 좋은 경로가 발견될 수 있음 -> 예외 처리 필수
+	// -- openList에서 찾아서 제거한다거나,
+	// -- pq에서 pop 한 다음에 무시한다거나,
+
+	// 초기값
+	{
+		int32 g = 0;
+		int32 h = 10 * (abs(dest.y - start.y) + abs(dest.x - start.x));
+		pq.push(PQNode{ g + h, g, start });
+		best[start.y][start.x] = g + h;
+		parent[start] = start;
+	}
+
+	while (pq.empty() == false)
+	{
+		// 제일 좋은 후보 찾기
+		PQNode node = pq.top();
+		pq.pop();
+
+		// 동일한 좌표를 여러 경로로 찾아서
+		// 이전에 더 빠른 경로로 이미 방문된 경우(closed) 스킵..
+
+		// [선택]
+		if (closed[node.pos.y][node.pos.x])
+			continue;
+		if (best[node.pos.y][node.pos.x] < node.f)
+			continue;
+
+		// 방문
+		closed[node.pos.y][node.pos.x] = true;
+
+		// 목적지에 도착했으면 바로 종료
+		if (node.pos == dest)
+			break;
+
+		for (int dir = 0; dir < DIR_COUNT; dir++)
+		{
+			Pos nextPos = node.pos + front[dir];
+
+			// 갈 수 있는 지역이 맞는지 확인
+			if (CanGo(nextPos) == false)
+				continue;
+			// [선택] 이미 방문한 곳이면 스킵
+			if (closed[nextPos.y][nextPos.x])
+				continue;
+
+			// 비용 계산
+			int32 g = node.g + cost[dir];
+			int32 h = 10 * (abs(dest.y - nextPos.y) + abs(dest.x - nextPos.x));
+
+			// 다른 경로에서 더 빠른 길을 찾았으면 스킵
+			if (best[nextPos.y][nextPos.x] <= g + h)
+				continue;
+
+			// 예약 진행
+			best[nextPos.y][nextPos.x] = g + h;
+			pq.push(PQNode{ g + h, g, nextPos });
+			parent[nextPos] = node.pos;
+		}
+	}
+
+	// 거꾸로 거슬러 올라가기
+	Pos pos = dest;
+
+	_path.clear();
+	_pathIndex = 0;
+
 	while (true)
 	{
 		_path.push_back(pos);
